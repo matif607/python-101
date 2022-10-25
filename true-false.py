534c4f8595b95a1586edb8458eed5a15cf6ded28
matif607 <mohammadatifurrehman_atif@comcast.com>
2022-10-25 16:49:26 +0530

    truefalse-condition

---
+++ true-false.py
@@ -0,0 +1,54 @@
+day = "Monday"
+temp = 30
+raining = True
+
+if day == "Saturday" and temp == 27 and not raining:
+    print("go swimming")
+else:
+    print("learn python")
+
+print()
+
+day = "Saturday"
+temp = 30
+raining = True
+
+if day == "Saturday" and temp == 27 and not raining:
+    print("go swimming")
+else:
+    print("learn python")
+
+print()
+
+day = "Saturday"
+temp = 27
+raining = True
+
+if day == "Saturday" and temp == 27 and not raining:
+    print("go swimming")
+else:
+    print("learn python")
+
+print()
+
+day = "Saturday"
+temp = 27
+raining = True
+
+if (day == "Friday" and temp == 27) or not raining:
+    print("go swimming")
+else:
+    print("learn python")
+
+print()
+
+day = "Saturday"
+temp = 27
+raining = False
+
+if (day == "Friday" and temp == 27) or not raining:
+    print("go swimming")
+else:
+    print("learn python")
+
+#always use parenthesis while using and & or together
